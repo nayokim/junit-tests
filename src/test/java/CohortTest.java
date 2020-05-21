4fba82869db6b67c377d89bc81e222f8f25dac4e
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-05-21 16:45:08 -0500

    postwalkthrough

--- src/test/java/CohortTest.java
+++ src/test/java/CohortTest.java
@@ -31,14 +31,31 @@ public class CohortTest {
     }
 
     @Test
+    public void testIfGetStudentsWork(){
+        Cohort apex = new Cohort();
+        assertEquals(0,apex.getStudents().size());
+        Student fer = new Student(1l, "Fer");
+        apex.addStudent(fer);
+        assertEquals(1,apex.getStudents().size());
+    }
+
+    @Test
     public void testAddStudentAndGetStudentsWork(){
         assertEquals(0, emptyCohort.getStudents().size());
         assertEquals(1, cohortWithOne.getStudents().size());
 //        assertEquals(1, cohortWithOne.getStudents().get(0).getId());
     }
 
+
     @Test
     public void testIfAvgIsCorrect(){
+        Cohort apex = new Cohort();
+        Student fer = new Student (1L, "fer");
+        fer.addGrade(100);
+        Student douglas = new Student (3l, "douglas");
+        douglas.addGrade(80);
+        apex.addStudent(fer);
+        apex.addStudent(douglas);
         assertEquals(95.0, cohortWithMany.getCohortAverage(), 0);
     }
 
